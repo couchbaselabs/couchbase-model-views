aa9909d8b9930e373ff33614df1aa865a6d70fb5
jzablocki <dev@zblock.net>
2012-10-23 11:22:44 -0400

    Updated demo project with Json.NET property mappers

--- src/CouchbaseModelViews.DemoModels/Beer.cs
+++ src/CouchbaseModelViews.DemoModels/Beer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CouchbaseModelViews.Framework.Attributes;
+using Newtonsoft.Json;
 
 namespace CouchbaseModelViews.DemoModels
 {
@@ -11,15 +12,19 @@ namespace CouchbaseModelViews.DemoModels
 	{
 		public string Id { get; set; }
 
+		[JsonProperty("name")]
 		[CouchbaseViewKey("by_abv_and_name", "name", 1)]
 		[CouchbaseViewKey("by_name", "name")]
 		public string Name { get; set; }
 
+		[JsonProperty("description")]
 		public string Description { get; set; }
 
+		[JsonProperty("abv")]
 		[CouchbaseViewKey("by_abv_and_name", "abv", 0)]
 		public float ABV { get; set; }
 
+		[JsonProperty("breweryId")]
 		[CouchbaseViewKey("by_brewery", "breweryId")]
 		public string Brewery { get; set; }
 	}
